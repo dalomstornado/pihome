5743a7d28a78440a79d7055ce4fb133dee23d280
dalomstornado <jonas@dehaan.se>
2017-01-30 21:48:06 +0100

    Adds telldus as a dependency.

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express()
 var path = require('path');
 var config = require('./config.json');
+var telldus = require('telldus');
 
 app.set('view engine', 'pug');
 app.locals.pretty = true;
@@ -19,6 +20,25 @@ app.get('/temperature/:sensorId', function(req, res){ //websockets
 });
 
 app.get('/', function(req, res){
+	telldus.turnOn(1,function(err) {
+	  console.log('1 is now ON');
+	});
+	telldus.turnOff(1,function(err) {
+	  console.log('2 is now OFF');
+	});
+
+	res.render('index', config);
+});
+
+app.get('/list', function(req, res){
+	telldus.getDevices(function(err,devices) {
+	  if ( err ) {
+	    console.log('Error: ' + err);
+	  } else {
+	    // The list of devices is returned
+	    console.log(devices);
+	  }
+	});
 	res.render('index', config);
 });
 
