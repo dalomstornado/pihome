bf510c50c054e99409d89675adacd33e53c1d300
jonash <ekst_jodeh@terra.local>
2017-03-14 16:50:00 +0100

    Adds jquery. Adds start of api.

---
+++ app/client/api.js
@@ -0,0 +1,11 @@
+const $ = require('jquery');
+
+const baseUrl = 'http://localhost:8082';
+
+/*
+const test = () => {
+	$.getJSON(baseUrl+'/temperatures/sensor1/2017-01-01'), (data) => {
+
+	}
+};
+*/
