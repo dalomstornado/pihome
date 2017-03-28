e923301c9f40260435492002c3d3d21bebdb188f
jonash <ekst_jodeh@terra.local>
2017-03-28 12:57:21 +0200

    Adds vind as a sensor with custom limits.

--- app/common/limits.js
+++ app/common/limits.js
@@ -2,17 +2,25 @@
 const LowerLimit = {
 	TEMPERATURE: { WARNING: 7, ALARM: 4 },
 	HUMIDITY: { WARNING: undefined, ALARM: undefined }, 
-	135: {
+	135: { //Ute
 		TEMPERATURE: { WARNING: -10, ALARM: -20 },
+		HUMIDITY: { WARNING: undefined, ALARM: undefined } 
+	},
+	136: { //Vind
+		TEMPERATURE: { WARNING: 0, ALARM: -10 },
 		HUMIDITY: { WARNING: undefined, ALARM: undefined } 
 	}
 };
 const UpperLimit = {
 	TEMPERATURE: { WARNING: 30, ALARM: 35 },
 	HUMIDITY: { WARNING: 65, ALARM: 75 },
-	135: {
+	135: { //Ute
 		TEMPERATURE: { WARNING: undefined, ALARM: undefined },
 		HUMIDITY: { WARNING: 90, ALARM: 93 } 
+	},
+	136: { //Vind
+		TEMPERATURE: { WARNING: undefined, ALARM: undefined },
+		HUMIDITY: { WARNING: 70, ALARM: 80 } 
 	}
 };
 
