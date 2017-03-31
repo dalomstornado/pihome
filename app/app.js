ca8752af040a11155162a2db2e3fe8bf659bf434
dalomstornado <jonas@dehaan.se>
2017-03-31 23:23:41 +0200

    Sets (among others) Access-Control header.

--- app/app.js
+++ app/app.js
@@ -8,12 +8,35 @@ const deviceHandler = require('./common/deviceHandler');
 const websocket = require('./server/websocket')(io);
 const telldusEventHandler = require('./server/telldusEventHandler')(websocket);
 
+const PORT = 8082;
+
 app.set('view engine', 'pug');
 app.locals.pretty = true;
 app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 app.use('/api', router);
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    const siteName = 'http://localhost:' + PORT;
+    res.setHeader('Access-Control-Allow-Origin', siteName);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 telldusEventHandler.init();
 
@@ -46,7 +69,6 @@ app.get('/list', function(req, res){
 	res.render('index', config);
 });
 
-const PORT = 8082;
 var server = http.listen(PORT, function () {
   console.log('Server listening on port %s', PORT);
 });
