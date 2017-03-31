efcb43d4901fc645a385f33d563179eab0d5166c
dalomstornado <jonas@dehaan.se>
2017-04-01 00:18:06 +0200

    Updates CORS settings.

--- app/app.js
+++ app/app.js
@@ -19,20 +19,18 @@ app.use('/api', router);
 
 // Add headers
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
-    const siteName = 'http://localhost:' + PORT;
-    res.setHeader('Access-Control-Allow-Origin', siteName);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS'); //'GET, POST, OPTIONS, PUT, PATCH, DELETE'
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next();
