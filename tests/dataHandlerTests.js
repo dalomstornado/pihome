318b036e3ebb827450b0660dcb9e28a48558938e
jonash <ekst_jodeh@terra.local>
2017-04-04 11:33:14 +0200

    Adds first unit test on dataHandler2.

--- tests/dataHandlerTests.js
+++ tests/dataHandlerTests.js
@@ -1,10 +1,23 @@
 const assert = require('assert');
 const expect = require('chai').expect;
+const dataHandler2 = require('../../app/client/dataHandler2');
+const moment = require('moment');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+const testDataSeries = [
+    [{date: moment().subtract(2, 'h').toDate(), value: 0},
+    {date: moment().subtract(1, 'h').toDate(), value: 5},
+    {date: moment().subtract(1, 'h').toDate(), value: 10},
+    {date: moment().subtract(0, 'h').toDate(), value: 20}],
+    [{date: moment().subtract(2, 'h').toDate(), value: 0},
+    {date: moment().subtract(1, 'h').toDate(), value: -10},
+    {date: moment().subtract(0, 'h').toDate(), value: -20}]
+];
+
+describe('DataHandler2', function() {
+  describe('LineChartData', function() {
+  	const lineChartData = dataHandler2.lineChartData(testDataSeries, moment().subtract(30, 'd'), 60);
+    it('should should have two sensors', function() {
+      assert.equal(2, lineChartData.length);
     });
   });
 });
